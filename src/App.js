b039950389068800ed8ff66289d6d7f3f156a07e
shourav <shouravs280@gmail.com>
2023-08-06 12:16:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+const App=() =>{
+  const [value,setValue]=useState(0);
+ const setHandler=(n)=>{
+  setValue(n=>n+1);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p id="value">{value}</p>
+         <button onMouseOver={setHandler}>Increment</button>
     </div>
   );
 }
